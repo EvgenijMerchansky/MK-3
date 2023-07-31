c7f788a3bfc551dc0c1e90d088b74cf6e084c24c
Evgenij Merchansky <evgenij.merchansky@gmail.com>
2023-07-31 16:46:48 +0300

    fix: move keyCode logic to the screen component

--- src/components/versusCodesGrid/index.tsx
+++ src/components/versusCodesGrid/index.tsx
@@ -1,34 +1,24 @@
-import { FC, useEffect, useState, useMemo } from 'react';
+import { FC, useEffect, useState } from 'react';
 import { VersusCodeModel } from '../../models/VersusCodeModel';
-import messages from '../../helpers/messages';
 import constants from '../../helpers/constants';
 import rotationSound from '../../assets/sounds/secret_code_rotation.mp3';
 import VersusCode from '../versusCode';
 import './index.scss';
 
 type StateType<T> = [T, React.Dispatch<React.SetStateAction<T>>];
-type VersusCodesGridProps = { versusCodes: VersusCodeModel[], defaultVersusCode: VersusCodeModel }
+type VersusCodesGridProps = { versusCodes: VersusCodeModel[], defaultVersusCode: VersusCodeModel, keyCode: string }
 
-const VersusCodesGrid: FC<VersusCodesGridProps> = ({ versusCodes, defaultVersusCode }) => {
+const VersusCodesGrid: FC<VersusCodesGridProps> = ({ versusCodes, defaultVersusCode, keyCode }) => {
     const rotationClassNames = 'rotation-active rotation-reverse-active';
-    const [key, setKey]: StateType<string> = useState<string>('');
     const [rotation, setRotation]: StateType<boolean> = useState<boolean>(false);
     const [selectedVersusCode, setSelectedVersusCode]: StateType<VersusCodeModel> = useState<VersusCodeModel>(defaultVersusCode);
 
     useEffect(() => {
-        document.addEventListener('keydown', handleKeyPress);
-        return (): void => {
-          document.removeEventListener('keydown', handleKeyPress);
-        };
-      }, []);
-
-    useEffect(() => {
-        if (key === '') return;
+        if (keyCode === '') return;
         if (rotation) return;
-        setKey(key);
         setRotation(rotation);
         rotateVersusCode();
-    }, [key, rotation]);
+    }, [keyCode, rotation]);
 
     useEffect(() => {
         setSelectedVersusCode(selectedVersusCode);
@@ -38,7 +28,7 @@ const VersusCodesGrid: FC<VersusCodesGridProps> = ({ versusCodes, defaultVersusC
 
     const rotateVersusCode = (): void => {
         versusCodes.forEach((versusCode: VersusCodeModel) => {
-            if (versusCode.key === key && versusCode.id !== selectedVersusCode.id && !rotation) {
+            if (versusCode.key === keyCode && versusCode.id !== selectedVersusCode.id && !rotation) {
                 setRotation(true);
                 setSelectedVersusCode(versusCode);
                 makeSound();
@@ -48,27 +38,6 @@ const VersusCodesGrid: FC<VersusCodesGridProps> = ({ versusCodes, defaultVersusC
             }
         });
     }
-
-    const handleKeyPress = ({ key }: KeyboardEvent): void => {
-        key = key.toLowerCase();
-        switch(key) {
-            case messages.q.defaultMessage:
-            case messages.w.defaultMessage:
-            case messages.e.defaultMessage:
-            case messages.r.defaultMessage:
-            case messages.t.defaultMessage:
-            case messages.y.defaultMessage:
-                setKey(key);
-            break;
-            default:
-            break;
-        }
-    }
-
-    // console.log('click', rotation);
-
-
-    // console.log(selectedVersusCode, 'selVerCode')
 
     return (
         <ul className='versus-codes'>
